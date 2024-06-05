f6d3463ed92bb02b750eef91bc47c9f55d9ce0f3
Fabian Henßler <Fabian.Henssler@wago.com>
2024-06-05 08:18:41 +0200

    Aufgabe fertig

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -1,4 +1,6 @@
 package streamapi;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import java.io.InputStream;
 import java.util.*;
@@ -59,20 +61,15 @@ public class Main {
     public static List<Integer> random() {
         Random r = new Random();
 
-        // TODO
-        List<Integer> randomIntegers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            randomIntegers.add(r.nextInt(10));
-        }
+        List<Integer> randomIntegers = IntStream.range(0, 10)
+            .map(i -> r.nextInt(10))
+            .boxed()
+            .toList();
 
-        List<Integer> returnList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            if (randomIntegers.get(i) % 2 == 0) {
-                returnList.add(randomIntegers.get(i) * randomIntegers.get(i));
-            }
-        }
-
-        return returnList;
+        return randomIntegers.stream()
+            .filter(i -> i % 2 == 0)
+            .map(i -> i * i)
+            .toList();
     }
 
     /**
